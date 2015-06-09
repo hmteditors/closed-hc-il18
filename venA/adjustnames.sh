30b93d534870e5d22e76d1da5080d1f751b84ec8
Neel Smith <dnsmith.neel@gmail.com>
2015-06-09 11:06:20 -0400

    Add DSE validation

---
+++ venA/adjustnames.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Substitute name of directory as required in various config files 
+# and scripts.
+SED=`which sed`
+GIT=`which git`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo USING $DIR for directory
+
+echo "Adjust paths to ${DIR}"
+
+
+
+${SED}  "s#PATH#$DIR/#g" configs/vm-mom-config.gradle-template > configs/vm-mom-config.gradle
+
+${SED} "s#PATH#$DIR/#g" scripts/dse.sh-template > scripts/dse.sh
+
+
+#git add collections/*.csv
+#git add collections/*.xml
+#git add collections/schemas/*.rng
+#git add configs/cdmgr.conf
+#git add configs/vm-mom-config.gradle
+#git add converted/*css
+#git add converted/README.md
+#git add editions/*xml
+#git add editions/Iliad/*xml
+#git add editions/schemas/*rng
+#git add editions/scholia/*xml
+#git add indices/*csv
+#git add scripts/*.sh
+#git add writing/README.md
+
+
